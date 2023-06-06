7bc424056f6c9fc10080c427bb0e596e7d52ed1a
swayam-agrahari <109474492+swayam-agrahari@users.noreply.github.com>
2023-06-07 02:12:43 +0545

    Update pattern04.cpp

--- C++/pattern04.cpp
+++ C++/pattern04.cpp
@@ -24,3 +24,45 @@ int main(){
     i++;
   } 
 }
+
+
+//  ___1
+//  __121
+//  _12321
+//  1234321
+
+#include<iostream>
+using namespace std;
+int main(){
+  int i=1,n;
+  cout<<"Enter the value of n:"<<endl;
+  cin>>n;
+  while(i<=n)
+  {
+    
+    int spaces=n-i;
+    while(spaces)
+    {
+      cout<<" ";
+      spaces--;
+    }
+    
+    int j=1;
+    while(j<=i)
+    {
+      cout<<j;
+      j++;
+    } 
+    
+    int k=i-1;
+    while(k)
+    {
+      cout<<k;
+      k--;
+      
+    }
+  
+    cout<<endl;
+    i++;
+  }
+}
