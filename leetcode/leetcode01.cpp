bfae4843d1ba579ada0e8c73d184291c0697d488
swayam-agrahari <109474492+swayam-agrahari@users.noreply.github.com>
2023-06-14 23:53:04 +0545

    Update leetcode01.cpp

--- leetcode/leetcode01.cpp
+++ leetcode/leetcode01.cpp
@@ -1,1 +1,19 @@
 
+//leetcode01
+// for an integer n, find difference of sum and product of its digits.
+
+#include<iostream>
+using namespace std;
+int main(){
+  int n,remainder,quotient,sum=0,product=1;
+  cout<<"Enter the number:"<<endl;
+  cin>>n;
+  while(n!=0){
+    remainder=n % 10;
+    quotient= n / 10;
+    sum=sum+remainder;
+    product=product*remainder;
+    n=quotient;   
+  }
+  cout<<"The difference is "<<product -sum<<endl;
+}
